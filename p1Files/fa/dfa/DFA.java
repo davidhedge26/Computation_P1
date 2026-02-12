ed9e0a515a4d679f0319ddaf31a27290d36be28b
JGuidry <jguids342@gmail.com>
2026-02-12 00:56:44 -0700

    Initialized 5 tuple for DFA and add state/setfinal

--- p1Files/fa/dfa/DFA.java
+++ p1Files/fa/dfa/DFA.java
@@ -1,42 +1,75 @@
 package fa.dfa;
 
+import java.util.LinkedHashSet;
+import java.util.LinkedHashMap;
 import java.util.Set;
 
 import fa.State;
 
 /**
  * DFA models and manages Definite Finite Automata
+ * 
  * @author David H, Jared G
  */
-public class DFA implements DFAInterface{
+public class DFA implements DFAInterface {
 
-	/**
-	 * Adds a a state to the FA instance
-	 * @param name is the label of the state 
-	 * @return true if a new state created successfully and false if there is already state with such name
-	 */
+    // 5 tuple:
+    // sigma is the alphabet
+    // states are the dfa state objects that currently just hold a name for a state
+    // init is just 1 singular dfa state
+    // final state is set as there could be multiple
+    // using Linked Hash versions of sets because these preserve order
+    // inner map uses character as key, dfa rules
+    // outter map results in end transition to next state
+    private LinkedHashSet<Character> sigma = new LinkedHashSet<>();
+    private LinkedHashSet<DFAState> states = new LinkedHashSet<>();
+    private DFAState init = new DFAState(null);
+    private LinkedHashSet<DFAState> finState = new LinkedHashSet<>();
+    private LinkedHashMap<Character, DFAState> innerdelta = new LinkedHashMap<>();
+    private LinkedHashMap<LinkedHashMap<Character, DFAState>, DFAState> outterdelta = new LinkedHashMap<>();
+
+    /**
+     * Adds a a state to the FA instance
+     * 
+     * @param name is the label of the state
+     * @return true if a new state created successfully and false if there is
+     *         already state with such name
+     */
     @Override
     public boolean addState(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addState'");
+
+        for (DFAState checker : this.states) {
+            if (checker.toString().equalsIgnoreCase(name))
+                return false;
+        }
+        this.states.add(new DFAState(name));
+        return true;
     }
 
     /**
-	 * Marks an existing state as an accepting state
-	 * @param name is the label of the state
-	 * @return true if successful and false if no state with such name exists
-	 */
+     * Marks an existing state as an accepting state
+     * 
+     * @param name is the label of the state
+     * @return true if successful and false if no state with such name exists
+     */
     @Override
     public boolean setFinal(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setFinal'");
+
+        for (DFAState checker : this.states) {
+            if (checker.toString().equalsIgnoreCase(name)) {
+                this.finState.add(checker);
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
-	 * Adds the initial state to the DFA instance
-	 * @param name is the label of the start state
-	 * @return true if successful and false if no state with such name exists
-	 */
+     * Adds the initial state to the DFA instance
+     * 
+     * @param name is the label of the start state
+     * @return true if successful and false if no state with such name exists
+     */
     @Override
     public boolean setStart(String name) {
         // TODO Auto-generated method stub
@@ -44,9 +77,10 @@ public class DFA implements DFAInterface{
     }
 
     /**
-	 * Adds a symbol to Sigma
-	 * @param symbol to add to the alphabet set
-	 */
+     * Adds a symbol to Sigma
+     * 
+     * @param symbol to add to the alphabet set
+     */
     @Override
     public void addSigma(char symbol) {
         // TODO Auto-generated method stub
@@ -54,11 +88,12 @@ public class DFA implements DFAInterface{
     }
 
     /**
-	 * Simulates a DFA on input s to determine
-	 * whether the DFA accepts s.
-	 * @param s - the input string
-	 * @return true if s in the language of the DFA and false otherwise
-	 */
+     * Simulates a DFA on input s to determine
+     * whether the DFA accepts s.
+     * 
+     * @param s - the input string
+     * @return true if s in the language of the DFA and false otherwise
+     */
     @Override
     public boolean accepts(String s) {
         // TODO Auto-generated method stub
@@ -66,9 +101,10 @@ public class DFA implements DFAInterface{
     }
 
     /**
-	 * Getter for Sigma
-	 * @return the alphabet of FA
-	 */
+     * Getter for Sigma
+     * 
+     * @return the alphabet of FA
+     */
     @Override
     public Set<Character> getSigma() {
         // TODO Auto-generated method stub
@@ -76,10 +112,11 @@ public class DFA implements DFAInterface{
     }
 
     /**
-	 * Returns state with the given name, or null if none exists
-	 * @param name of a state
-	 * @return state object or null
-	 */
+     * Returns state with the given name, or null if none exists
+     * 
+     * @param name of a state
+     * @return state object or null
+     */
     @Override
     public State getState(String name) {
         // TODO Auto-generated method stub
@@ -87,10 +124,11 @@ public class DFA implements DFAInterface{
     }
 
     /**
-	 * Determines if a state with a given name is final
-	 * @param name the name of the state
-	 * @return true if a state with that name exists and it is final
-	 */
+     * Determines if a state with a given name is final
+     * 
+     * @param name the name of the state
+     * @return true if a state with that name exists and it is final
+     */
     @Override
     public boolean isFinal(String name) {
         // TODO Auto-generated method stub
@@ -98,23 +136,26 @@ public class DFA implements DFAInterface{
     }
 
     /**
-	 * Determines if a state with name is final
-	 * @param name the name of the state
-	 * @return true if a state with that name exists and it is the start state
-	 */
+     * Determines if a state with name is final
+     * 
+     * @param name the name of the state
+     * @return true if a state with that name exists and it is the start state
+     */
     @Override
     public boolean isStart(String name) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'isStart'");
     }
 
-	/**
-	 * Adds the transition to the DFA's delta data structure
-	 * @param fromState is the label of the state where the transition starts
-	 * @param toState is the label of the state where the transition ends
-	 * @param onSymb is the symbol from the DFA's alphabet.
-	 * @return true if successful and false if one of the states don't exist or the symbol in not in the alphabet
-	 */
+    /**
+     * Adds the transition to the DFA's delta data structure
+     * 
+     * @param fromState is the label of the state where the transition starts
+     * @param toState   is the label of the state where the transition ends
+     * @param onSymb    is the symbol from the DFA's alphabet.
+     * @return true if successful and false if one of the states don't exist or the
+     *         symbol in not in the alphabet
+     */
     @Override
     public boolean addTransition(String fromState, String toState, char onSymb) {
         // TODO Auto-generated method stub
@@ -122,36 +163,38 @@ public class DFA implements DFAInterface{
     }
 
     /**
-	 * Creates a deep copy of this DFA
-	 * which transitions labels are
-	 * swapped between symbols symb1
-	 * and symb2.
-	 * @return a copy of this DFA
-	 */
+     * Creates a deep copy of this DFA
+     * which transitions labels are
+     * swapped between symbols symb1
+     * and symb2.
+     * 
+     * @return a copy of this DFA
+     */
     @Override
     public DFA swap(char symb1, char symb2) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'swap'");
     }
-    
+
     /**
-	 * Construct the textual representation of the DFA, for example
-	 * A simple two state DFA
-	 * Q = { a b }
-	 * Sigma = { 0 1 }
-	 * delta =
-	 *		0	1	
-	 *	a	a	b	
-	 *	b	a	b	
-	 * q0 = a
-	 * F = { b }
-	 * 
-	 * The order of the states and the alphabet is the order
-	 * in which they were instantiated in the DFA.
-	 * @return String representation of the DFA
-	 */
+     * Construct the textual representation of the DFA, for example
+     * A simple two state DFA
+     * Q = { a b }
+     * Sigma = { 0 1 }
+     * delta =
+     * 0 1
+     * a a b
+     * b a b
+     * q0 = a
+     * F = { b }
+     * 
+     * The order of the states and the alphabet is the order
+     * in which they were instantiated in the DFA.
+     * 
+     * @return String representation of the DFA
+     */
     @Override
-    public String toString(){
+    public String toString() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'swap'");
     }
